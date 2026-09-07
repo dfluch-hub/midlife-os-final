f651a702ad3ec5703ca28170b8191b488ec9e151
dfluch-hub <d.fluch@gmx.at>
2026-09-08 01:54:35 +0200

    Cache final Midlife OS v14 runtime

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='midlife-os-v14';
-const ASSETS=['./','./index.html','./styles-v14.css?v=14','./app-v14.js?v=14','./manifest.webmanifest?v=14','./icon.svg'];
+const CACHE='midlife-os-v14b';
+const ASSETS=['./','./index.html','./styles-v14.css?v=14','./app-v14b.js?v=14','./manifest.webmanifest?v=14','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
