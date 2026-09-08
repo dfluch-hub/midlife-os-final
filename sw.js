326ceff195045900044c32a53d5eede6a1cdd10d
dfluch-hub <d.fluch@gmx.at>
2026-09-08 09:37:44 +0200

    Bump Midlife OS service worker to v18

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='midlife-os-v17';
-const ASSETS=['./','./index.html','./styles-v17.css?v=17','./app-v17.js?v=17','./manifest.webmanifest?v=17','./icon.svg'];
+const CACHE='midlife-os-v18';
+const ASSETS=['./','./index.html','./styles-v18.css?v=18','./app-v18.js?v=18','./manifest.webmanifest?v=18','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
